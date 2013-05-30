962572af07c9e399869968432bd4d734347f06a2
Iwasaki Yudai <yudai@arielworks.com>
2013-05-30 14:38:11 +0900

    Make postinstall script re-runnable

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -ex
 
 # gems
-gem install cf --no-rdoc --no-ri
-gem install admin-cf-plugin --no-rdoc --no-ri
+gem install cf admin-cf-plugin --no-rdoc --no-ri
 
 # Disable nginx
 # https://github.com/cloudfoundry/cf-release/pull/81
 sudo sed  -i -e "s/check process nginx_ccng.*$//" /var/vcap/monit/job/0001_micro_ng.cloud_controller_ng.monitrc
 
 # NFS for SDS
+export_line='/cfsnapshot     127.0.0.1(rw,sync,no_subtree_check)'
 sudo apt-get install nfs-kernel-server
-sudo sh -c "echo '/cfsnapshot     127.0.0.1(rw,sync,no_subtree_check)' >> /etc/exports"
-sudo mkdir /cfsnapshot
+if ! (grep -q "${export_line}" /etc/exports); then
+    sudo sh -c "echo '${export_line}' >> /etc/exports"
+fi
+sudo mkdir -p /cfsnapshot
 sudo /etc/init.d/nfs-kernel-server restart
