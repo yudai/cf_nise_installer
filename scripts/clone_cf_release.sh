b865332348751c832f88eabba50e0e3430700578
Iwasaki Yudai <yudai@arielworks.com>
2013-06-02 18:45:49 +0900

    Add variable for specifying url for cf-release

--- scripts/clone_cf_release.sh
+++ scripts/clone_cf_release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 
+CF_RELEASE_URL=${CF_RELEASE_URL:-https://github.com/cloudfoundry/cf-release.git}
 CF_RELEASE_BRANCH=${CF_RELEASE_BRANCH:-master}
 
 if [ ! -d cf-release ]; then
-    git clone --depth 1 https://github.com/cloudfoundry/cf-release.git
+    git clone --depth 1 ${CF_RELEASE_URL}
 fi
 
 (
