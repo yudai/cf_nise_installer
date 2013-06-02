e96730bde2a3798a910f270e9c85f0d67956fd16
Iwasaki Yudai <yudai@arielworks.com>
2013-06-02 21:32:18 +0900

    Check out specified branch on cloning

--- scripts/clone_cf_release.sh
+++ scripts/clone_cf_release.sh
@@ -4,12 +4,11 @@ CF_RELEASE_URL=${CF_RELEASE_URL:-https://github.com/cloudfoundry/cf-release.git}
 CF_RELEASE_BRANCH=${CF_RELEASE_BRANCH:-master}
 
 if [ ! -d cf-release ]; then
-    git clone --depth 1 ${CF_RELEASE_URL}
+    git clone --depth 1 -b ${CF_RELEASE_BRANCH} ${CF_RELEASE_URL}
 fi
 
 (
     cd cf-release
-    git checkout -b ${CF_RELEASE_BRANCH}
     ./update
     bosh -n create release --force
 )
