469be434f7cdd54f5971452d093b019cdb424300
Iwasaki Yudai <yudai@arielworks.com>
2013-06-04 12:31:46 +0900

    Use debian_nfs_server for NFS

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,14 +3,5 @@
 # gems
 gem install cf admin-cf-plugin --no-rdoc --no-ri
 
-# NFS for SDS
-export_line='/cfsnapshot     127.0.0.1(rw,sync,no_subtree_check)'
-sudo apt-get install nfs-kernel-server
-if ! (grep -q "${export_line}" /etc/exports); then
-    sudo sh -c "echo '${export_line}' >> /etc/exports"
-fi
-sudo mkdir -p /cfsnapshot
-sudo /etc/init.d/nfs-kernel-server restart
-
 # launch monit daemon
 sudo /var/vcap/bosh/bin/monit
