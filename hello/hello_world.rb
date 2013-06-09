6d73bcb70e3d54400a8ec20af4d80ee06b3efd80
Iwasaki Yudai <yudai@arielworks.com>
2013-06-09 22:07:23 +0900

    Connect to bound MySQL server for testing

--- hello/hello_world.rb
+++ hello/hello_world.rb
@@ -1,6 +1,10 @@
 require "sinatra"
+require "mysql2"
+require "cf-autoconfig"
+
 class HelloWorld < Sinatra::Base
   get "/" do
+    client = Mysql2::Client.new(:host => "localhost", :username => "root")
     "Hello, World!"
   end
 end
