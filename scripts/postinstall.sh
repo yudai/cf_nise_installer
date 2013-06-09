dd7dafbca168a67a13f421a2152d7359bdd26d35
Iwasaki Yudai <yudai@arielworks.com>
2013-06-09 22:16:38 +0900

    Remove and replace IP address restrictions for service nodes
    Service nodes work in a warden container, applications connect to the nodes from other than 127.0.0.1.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -12,5 +12,12 @@ fi
 sudo mkdir -p /cfsnapshot
 sudo /etc/init.d/nfs-kernel-server restart
 
+# remove unnecessary restriction
+sudo sed -i "s/bind-address            = 127.0.0.1//g" /var/vcap/jobs/mysql_node_ng/config/my.cnf
+sudo sed -i "s/bind-address            = 127.0.0.1//g" /var/vcap/jobs/mysql_node_ng/config/my55.cnf
+sudo sed -i "s/listen_addresses = '127.0.0.1'/listen_addresses = '*'/g" /var/vcap/jobs/postgresql_node_ng/config/postgresql.conf
+sudo sed -i "s/listen_addresses = '127.0.0.1'/listen_addresses = '*'/g" /var/vcap/jobs/postgresql_node_ng/config/postgresql91.conf
+sudo sed -i "s/listen_addresses = '127.0.0.1'/listen_addresses = '*'/g" /var/vcap/jobs/postgresql_node_ng/config/postgresql92.conf
+
 # launch monit daemon
 sudo /var/vcap/bosh/bin/monit
