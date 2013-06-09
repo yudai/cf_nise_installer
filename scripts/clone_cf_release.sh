dcb3a3ec562722093febf162634e57ae657b251f
Iwasaki Yudai <yudai@arielworks.com>
2013-06-09 22:38:15 +0900

    Checkout specified branch/revision after cloning
    The `clone -b` option does not support revision.

--- scripts/clone_cf_release.sh
+++ scripts/clone_cf_release.sh
@@ -4,11 +4,15 @@ CF_RELEASE_URL=${CF_RELEASE_URL:-https://github.com/cloudfoundry/cf-release.git}
 CF_RELEASE_BRANCH=${CF_RELEASE_BRANCH:-master}
 
 if [ ! -d cf-release ]; then
-    git clone --depth 1 -b ${CF_RELEASE_BRANCH} ${CF_RELEASE_URL}
+    git clone ${CF_RELEASE_URL}
+    (
+        cd cf-release
+        git checkout ${CF_RELEASE_BRANCH}
+    )
 fi
 
 (
     cd cf-release
-    ./update
+    git submodule foreach --recursive git submodule sync && git submodule update --init --recursive
     bosh -n create release --force
 )
