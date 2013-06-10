b17531d4e1280b22df02407541104bc2da6bbbcf
Iwasaki Yudai <yudai@arielworks.com>
2013-06-10 16:12:04 +0900

    Make timeout for process start up longer

--- scripts/start_processes.sh
+++ scripts/start_processes.sh
@@ -5,7 +5,7 @@ sudo /var/vcap/bosh/bin/monit
 sudo /var/vcap/bosh/bin/monit start all
 
 echo "Waiting for all processes to start"
-for ((i=0; i < 60; i++)); do
+for ((i=0; i < 120; i++)); do
     if (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -E "running$"); then
         break
     fi
