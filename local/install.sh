e6ad54c3ca745f05a34f13e513237d384988a64f
Iwasaki Yudai <yudai@arielworks.com>
2013-06-19 21:21:21 +0900

    Use .profile to initialize rbenv

--- local/install.sh
+++ local/install.sh
@@ -19,10 +19,10 @@ fi
 if [ ! -d ~/.rbenv ]; then
     git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
     git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+    echo 'eval "$(rbenv init -)"' >> ~/.profile
 fi
-. ~/.bash_profile
+. ~/.profile
 if ! (rbenv versions | grep -q 1.9.3-p392); then
     rbenv install 1.9.3-p392
 fi
