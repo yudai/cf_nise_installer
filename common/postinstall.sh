b8a72156189a8fae680408fbf4017c380ab1c691
Iwasaki Yudai <yudai@arielworks.com>
2013-06-22 02:02:50 +0900

    Remove services and add login, collector and ad-hock posgres

--- common/postinstall.sh
+++ common/postinstall.sh
@@ -2,18 +2,37 @@
 
 # Be careful, this file may be a symbolic link
 
-# NFS for SDS
-export_line='/cfsnapshot     127.0.0.1(rw,sync,no_subtree_check)'
-sudo apt-get install nfs-kernel-server
-if ! (grep -q "${export_line}" /etc/exports); then
-    sudo sh -c "echo '${export_line}' >> /etc/exports"
-fi
-sudo mkdir -p /cfsnapshot
-sudo /etc/init.d/nfs-kernel-server restart
+port=2545
+sudo apt-get install -y postgresql postgresql-contrib
+sudo sed -i "s/^port = 5432/port = ${port}/" /etc/postgresql/8.4/main/postgresql.conf
+sudo sed -i "s/^#listen_addresses/listen_addresses/" /etc/postgresql/8.4/main/postgresql.conf
 
-# remove unnecessary restriction
-sudo sed -i "s/bind-address            = 127.0.0.1//g" /var/vcap/jobs/mysql_node_ng/config/my.cnf
-sudo sed -i "s/bind-address            = 127.0.0.1//g" /var/vcap/jobs/mysql_node_ng/config/my55.cnf
-sudo sed -i "s/listen_addresses = '127.0.0.1'/listen_addresses = '*'/g" /var/vcap/jobs/postgresql_node_ng/config/postgresql.conf
-sudo sed -i "s/listen_addresses = '127.0.0.1'/listen_addresses = '*'/g" /var/vcap/jobs/postgresql_node_ng/config/postgresql91.conf
-sudo sed -i "s/listen_addresses = '127.0.0.1'/listen_addresses = '*'/g" /var/vcap/jobs/postgresql_node_ng/config/postgresql92.conf
+cat <<EOF | sudo tee /etc/postgresql/8.4/main/pg_hba.conf
+local   all             all                                     trust
+host    all             all             127.0.0.1/32            trust
+host    all             all             ::1/128                 trust
+host    all             all             0.0.0.0/0               md5
+EOF
+
+for role in root admin; do
+    echo "host    all  ${role} 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/8.4/main/pg_hba.conf
+done
+
+sudo /etc/init.d/postgresql-8.4 restart
+
+sleep 60
+
+for role in root uaa; do
+    psql -U postgres -p ${port} -d postgres -c "CREATE ROLE \"${role}\""
+    psql -U postgres -p ${port} -d postgres -c "ALTER ROLE \"${role}\" WITH LOGIN PASSWORD ''"
+done
+
+for db in appcloud_ng uaa; do
+    sudo -u postgres createdb "${db}" -p ${port}
+    psql -U postgres -p ${port} -d "${db}" -f /usr/share/postgresql/8.4/contrib/citext.sql
+done
+
+
+sudo sed -i "s/check process vcap_registrar/check process vcap_registrar_login/" /var/vcap/monit/job/0001_micro.login.monitrc
+sudo sed -i "s/vcap_registrar.pid/vcap_registrar_login.pid/" /var/vcap/monit/job/0001_micro.login.monitrc
+sudo sed -i "s/vcap_registrar.pid/vcap_registrar_login.pid/" /var/vcap/jobs/login/bin/vcap_registrar_ctl
--- hello/hello_world.rb
+++ hello/hello_world.rb
@@ -1,10 +1,7 @@
 require "sinatra"
-require "mysql2"
-require "cf-autoconfig"
 
 class HelloWorld < Sinatra::Base
   get "/" do
-    client = Mysql2::Client.new(:host => "localhost", :username => "root")
     "Hello, World!"
   end
 end
