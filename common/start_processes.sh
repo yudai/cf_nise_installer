07c942235cf687312e2ea3efee8da2f9b89dc7ae
Iwasaki Yudai <yudai@arielworks.com>
2013-06-27 17:58:01 +0900

    Invoke processes in order
    Sometimes CCNG fails to start at the first run when invoked with `start all`.

--- common/start_processes.sh
+++ common/start_processes.sh
@@ -2,7 +2,31 @@
 
 sudo /var/vcap/bosh/bin/monit
 sleep 5
-sudo /var/vcap/bosh/bin/monit start all
+
+for process in \
+    postgres \
+    nats \
+    cloud_controller_ng;
+do
+    sudo /var/vcap/bosh/bin/monit start $process
+    sleep 30
+done;
+
+for process in \
+    warden \
+    dea_next \
+    dir_server \
+    health_manager_next \
+    uaa \
+    uaa_vcap_registrar \
+    login \
+    login_vcap_registrar \
+    gorouter \
+    collector;
+do
+    sudo /var/vcap/bosh/bin/monit start $process
+    sleep 5
+done
 
 echo "Waiting for all processes to start"
 for ((i=0; i < 120; i++)); do
