40142bf4dcf8f77997938541d9c06447e9ad0aec
Iwasaki Yudai <iwasaki.yudai@lab.ntt.co.jp>
2013-08-08 18:43:10 +0900

    Fix typo in the command to retrieve ruby version

--- common/clone_cf_release.sh
+++ common/clone_cf_release.sh
@@ -4,7 +4,7 @@ CF_RELEASE_URL=${CF_RELEASE_URL:-https://github.com/cloudfoundry/cf-release.git}
 CF_RELEASE_BRANCH=${CF_RELEASE_BRANCH:-master}
 CF_RELEASE_USE_HEAD=${CF_RELEASE_USE_HEAD:-no}
 
-ruby_version=`benv version | cut -f1 -d" "` # overwrite .ruby-version
+ruby_version=`rbenv version | cut -f1 -d" "` # overwrite .ruby-version
 
 if [ ! -d cf-release ]; then
     git clone ${CF_RELEASE_URL}
