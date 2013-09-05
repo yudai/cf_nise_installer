ec59de1ef3e95608e958c545fa79cf256fc4cba1
Cornelia Davis <cdavis@gopivotal.com>
2013-09-04 22:12:39 -0700

    Added logic to wait on stopping postgres until everything else was stopped. Added status messages.wq

--- common/stop_processes.sh
+++ common/stop_processes.sh
@@ -1,15 +1,7 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 sudo /var/vcap/bosh/bin/monit
 sleep 5
-
-for process in \
-    postgres \
-    nats \
-    cloud_controller_ng;
-do
-    sudo /var/vcap/bosh/bin/monit stop $process
-done;
 
 for process in \
     nginx_ccng \
@@ -19,7 +11,36 @@ for process in \
     uaa_cf-registrar \
     warden \
     dea_next \
-    dir_server;
+    dir_server \
+    cloud_controller_ng \
+    nats;
 do
     sudo /var/vcap/bosh/bin/monit stop $process
 done
+
+echo "Waiting for all processes but postgres to stop"
+for ((i=0; i < 24; i++)); do
+    COUNT=$(sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -c -E "stop pending$")
+    if [ $COUNT == 0 ]; then
+        break
+    fi
+    sleep 5
+done
+
+COUNT=$(sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -c -E "stop pending$")
+if [ $COUNT != 0 ]; then
+  echo "Unable to stop processes"
+else 
+  echo "Now stopping postgres..."
+  sudo /var/vcap/bosh/bin/monit stop postgres
+  for ((i=0; i < 12; i++)); do
+    COUNT=$(sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -c -E "stop pending$")
+    if [ $COUNT == 0 ]; then
+        break
+    fi
+    sleep 5
+  done
+  if [ $COUNT != 0 ]; then
+    echo "Unable to stop postgres processes"
+  fi
+fi
