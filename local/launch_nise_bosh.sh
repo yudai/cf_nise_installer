10a0a0bed0dff0c8ef5eaf0f42f16279ce797f8d
Noburou TANIGUCHI <dev@nota.m001.jp>
2013-10-31 23:03:00 +0900

    Fix typo with script file name

--- local/launch_nise_bosh.sh
+++ local/launch_nise_bosh.sh
@@ -3,7 +3,7 @@
 # Set current ip to nats server
 NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
 
-NISE_IP_ADDRESS=${NISE_IP_ADDRESS} ./common/launch_nsie_bosh.sh
+NISE_IP_ADDRESS=${NISE_IP_ADDRESS} ./common/launch_nise_bosh.sh
 
 (
     cd nise_bosh
