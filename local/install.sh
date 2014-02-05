9e9036c9be94b495c9af78c63832af4bd84da26f
Iwasaki Yudai <yudai@arielworks.com>
2014-02-05 12:58:08 +0900

    Use ruby 1.9.3-p484

--- local/install.sh
+++ local/install.sh
@@ -24,13 +24,14 @@ if [ ! -d ~/.rbenv ]; then
     echo 'eval "$(rbenv init -)"' >> ~/.profile
 fi
 . ~/.profile
-if ! (rbenv versions | grep -q 1.9.3-p448); then
-    rbenv install 1.9.3-p448
+if ! (rbenv versions | grep -q 1.9.3-p484); then
+    rbenv install 1.9.3-p484
 fi
-rbenv local 1.9.3-p448
+rbenv local 1.9.3-p484
 
 # BOSH CLI and CF commands
 gem install bundler cf --no-rdoc --no-ri
+gem install rake -v 0.9.2.2 # hack for collector
 rbenv rehash
 
 # cf-release
