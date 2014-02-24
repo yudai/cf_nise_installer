2cb4c9e63862e0dbc66dbba62801159eec378900
Iwasaki Yudai <yudai@arielworks.com>
2014-02-24 11:51:38 +0900

    Add manfiest for 159
    Cleaning up manifest

--- local/launch_nise_bosh.sh
+++ local/launch_nise_bosh.sh
@@ -5,6 +5,8 @@ NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d
 
 NISE_IP_ADDRESS=${NISE_IP_ADDRESS} ./common/launch_nise_bosh.sh
 
+WITH_PRIVILEGE=${WITH_PRIVILEGE:-sudo env PATH=$PATH}
+
 (
     cd nise_bosh
     if [ "" != "$NISE_BOSH_REV" ]; then
@@ -13,7 +15,7 @@ NISE_IP_ADDRESS=${NISE_IP_ADDRESS} ./common/launch_nise_bosh.sh
     echo Use nise_bosh of revision: `git rev-list --max-count=1 HEAD` in $0
     bundle install
     # Old spec format
-    sudo env PATH=$PATH bundle exec ./bin/nise-bosh -y ../cf-release ../.deploy.yml micro -n ${NISE_IP_ADDRESS}
+    ${WITH_PRIVILEGE} bundle exec ./bin/nise-bosh -y ../cf-release ../.deploy.yml micro -n ${NISE_IP_ADDRESS}
     # New spec format, keeping the  monit files installed in the previous run
-    sudo env PATH=$PATH bundle exec ./bin/nise-bosh --keep-monit-files -y ../cf-release ../.deploy.yml micro_ng -n ${NISE_IP_ADDRESS}
+    ${WITH_PRIVILEGE} bundle exec ./bin/nise-bosh --keep-monit-files -y ../cf-release ../.deploy.yml micro_ng -n ${NISE_IP_ADDRESS}
 )
