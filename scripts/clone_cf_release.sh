2838acc234de8d08893a8036c9a1982a081af48c
Iwasaki Yudai <iwasaki.yudai@lab.ntt.co.jp>
2014-02-26 22:31:56 +0900

    Use submodule to checkout cf-release to lock cf-release revisions

--- scripts/clone_cf_release.sh
+++ scripts/clone_cf_release.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -ex
 
-CF_RELEASE_URL=${CF_RELEASE_URL:-https://github.com/cloudfoundry/cf-release.git}
-CF_RELEASE_BRANCH=${CF_RELEASE_BRANCH:-master}
 CF_RELEASE_USE_HEAD=${CF_RELEASE_USE_HEAD:-no}
 
-ruby_version=`rbenv version | cut -f1 -d" "` # overwrite .ruby-version
+ruby_version=`rbenv version | cut -f1 -d" "` # to overwrite .ruby-version
 
 if [ ! -d cf-release ]; then
-    git clone ${CF_RELEASE_URL}
+    if [ -z "${CF_RELEASE_URL}" ]; then
+        git submodule update --init cf-release
+    else
+        git clone ${CF_RELEASE_URL} cf-release
+    fi
+
     (
         cd cf-release
-        git checkout -f ${CF_RELEASE_BRANCH}
+
+        if [ -z "${CF_RELEASE_BRANCH}" ]; then
+            git checkout -f ${CF_RELEASE_BRANCH}
+        fi
 
         if [ $CF_RELEASE_USE_HEAD != "no" ]; then
             git submodule update --init --recursive
@@ -18,4 +24,6 @@ if [ ! -d cf-release ]; then
             RBENV_VERSION=$ruby_version bundle exec bosh -n create release --force
         fi
     )
+else
+    echo "'cf-release' directory exists. Skipping cloning..."
 fi
