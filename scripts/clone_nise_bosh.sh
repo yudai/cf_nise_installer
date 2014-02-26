c2bb1f5407ef1ce679998afa17493c8ddd1bd432
Iwasaki Yudai <iwasaki.yudai@lab.ntt.co.jp>
2014-02-26 22:31:26 +0900

    Checkout specified revision only when directory does not exist

--- scripts/clone_nise_bosh.sh
+++ scripts/clone_nise_bosh.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -ex
 
-git submodule update --init --recursive
-(
-    cd nise_bosh
-    if [ "" != "$NISE_BOSH_REV" ]; then
-        git checkout $NISE_BOSH_REV
-    fi
-    echo Use nise_bosh of revision: `git rev-list --max-count=1 HEAD` in $0
-)
+if [ ! -d nise_bosh ]; then
+    git submodule update --init --recursive nise_bosh
+
+
+    (
+        cd nise_bosh
+
+        if [ "" != "$NISE_BOSH_REV" ]; then
+            git checkout $NISE_BOSH_REV
+        fi
+
+        echo "Using Nise BOSH revision: `git rev-list --max-count=1 HEAD`"
+    )
+else
+    echo "'nise_bosh' directory exists. Skipping cloning..."
+fi
