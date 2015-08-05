2207a8be55ef3d4f618b71218bb59a8e4db60b7e
Iwasaki Yudai <yudai.iwasaki@ntti3.com>
2015-08-05 13:13:35 -0700

    Consider accessible files as ready

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,13 +19,13 @@ sudo /var/vcap/bosh/bin/monit start all
 
 echo "Waiting for all processes to start"
 for ((i=0; i < 120; i++)); do
-    if ! (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -v -E "running$"); then
+    if ! (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -v -E "(running|accessible)$"); then
         break
     fi
     sleep 10
 done
 
-if (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -v -E "running$"); then
+if (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -v -E "(running|accessible)$"); then
     echo "Found process failed to start"
     exit 1
 fi
